1df733afcd01e648c1c853a61c27a0f98277de50
jamilagastar <jamilagastar@gmail.com>
2022-08-15 18:57:16 +1000

    partially completed lab

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_list)
+    name_list.map{|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(attendee_name)
+    attendee_name.each.with_index(1).collect {|name, room_assignments| 
+       "Hello, #{name}! You'll be assigned to room #{room_assignments}!"}
+end
+
+assign_rooms(["Jamila", "Gaia", "Maybel"])
+
+def printer(batch_badge_creator, assign_rooms)
+    batch_badge_creator.map{|name| puts "Hello, my name is #{name}"}
+end
